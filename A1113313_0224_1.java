a013bf3e72e83761a66eea1e18d08076d04475b0
Feng-OwO <125659986+Feng-OwO@users.noreply.github.com>
2023-03-01 13:54:00 +0800

    Add files via upload

---
+++ A1113313_0224_1.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class A1113313_0224_1{
+    public static void main(String[]argv){
+        int x=0;
+        System.out.println("請輸入整數");
+        Scanner sc=new Scanner(System.in);
+        x=sc.nextInt();
+
+       if(x%2==0){
+        System.out.println("是偶數");
+       }else{
+        System.out.println("是奇數");
+       }
+    
+        
+    }
+}
---
+++ A1113313_0224_2.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class A1113313_0224_2{
+    public static void main(String[]argv){
+        float C,F;
+        System.out.println("請輸入攝氏溫度");
+        Scanner sc= new Scanner(System.in);
+        C=sc.nextFloat();
+        F=C*9/5+32;
+        System.out.println("華氏溫度為"+F+"度");
+
+        }
+}
